31e84239efdc5f0aacbb209607499c0e48b9d548
Subhradeep10 <subhradeepsamanta2002@gmail.com>
2021-09-30 23:31:03 +0530

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        "/": (context) => Homepage(),
+        "/": (context) => LoginPage(),
         '/LoginPage': (context) => Homepage(),
       },
     );
--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,18 +7,56 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.blueAccent,
-      child: Center(
-        child: Center(
-          child: Text(
-            "Login Page",
-            style: TextStyle(
-                fontSize: 24.0,
-                color: Colors.white,
-                fontWeight: FontWeight.w400),
-          ),
-        ),
-      ),
-    );
+        child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.purple, Colors.blue],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.bottomRight)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/login.png",
+                  fit: BoxFit.cover,
+                  scale: 5,
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                Text(
+                  "Welcome to Login Screen",
+                  style: TextStyle(color: Colors.white, fontSize: 26),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        hintText: "username@gmail.com",
+                        labelText: "Username",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        labelText: "Password",
+                      ),
+                    ),
+                  ]),
+                )
+              ],
+            )));
   }
 }
